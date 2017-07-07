0bf9623e0753946576ce09ad06865d33d21cdc28
Love Borgström <loveborgstrom98@gmail.com>
2017-07-07 19:54:41 +0200

    Better error handling

--- b64gz.sh
+++ b64gz.sh
@@ -4,6 +4,12 @@
 
 # This script generates a base64 encoded gzip string, which outputs to stdout, whose main goal is to be able to include longer code snippets as a one line in case of complicated script structure resulting in failed minification with normal methods.
 
+if [ "$(caller 0)" != "" ];then
+	exit_mode="return"
+else
+	exit_mode="exit"
+fi
+
 if [ "$1" != "" ] && [ "$2" != "" ];then		
 	if [ "$1" == "file" ];then
 		mode="file"
@@ -11,11 +17,11 @@ if [ "$1" != "" ] && [ "$2" != "" ];then
 		mode="stdin"
 	else
 		echo "Unknown \$1. Supported ones are 'file' and 'stdin'"
-		exit 1
+		$exit_mode "1"
 	fi	
 elif [ "$1" != "file" ] || [ "$1" != "stdin" ];then
 	echo "Unknown \$1. Supported ones are 'file' and 'stdin'"
-	exit 1
+	$exit_mode "1"
 else
 	echo -e "No input."
 fi
@@ -25,7 +31,7 @@ if [ "$mode" == "file" ];then
 		cat "$2" | gzip -9 | base64 
 	else
 		echo "File '$2' does not exist"
-		exit 2
+		$exit_mode "2"
 	fi
 elif [ "$mode" == "stdin" ];then
 	shift
